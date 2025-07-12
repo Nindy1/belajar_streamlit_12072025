58204eea89fb46438212d9408839e0a2f831dd64
Nindy1 <nindyaghinaramadhani@gmail.com>
2025-07-12 09:23:22 +0700

    Update app.py

--- app.py
+++ app.py
@@ -10,3 +10,15 @@ if st.button("Say hello"):
 import streamlit as st
 
 st.link_button("Go to gallery", "https://streamlit.io/gallery")
+import streamlit as st
+st.image("sunrise.jpg", caption="Sunrise by the mountains")
+import streamlit as st
+import time
+
+with st.sidebar:
+    with st.echo():
+        st.write("This code will be printed to the sidebar.")
+
+    with st.spinner("Loading..."):
+        time.sleep(5)
+    st.success("Done!")
